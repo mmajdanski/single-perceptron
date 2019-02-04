ba6f98acefebbefc2caf947ffe1a4c3a72443d7d
mmajdanski <michael.majdanski@gmail.com>
2019-02-03 19:10:55 -0800

    Updates

---
+++ precache-manifest.0036ea18c051a64c1da767841e2cdd66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "202b8fab8d8f151e3f21",
+    "url": "/single-perceptron/static/js/runtime~main.202b8fab.js"
+  },
+  {
+    "revision": "6c64f64141bd3066713e",
+    "url": "/single-perceptron/static/js/main.6c64f641.chunk.js"
+  },
+  {
+    "revision": "1305802b23d579707d69",
+    "url": "/single-perceptron/static/js/1.1305802b.chunk.js"
+  },
+  {
+    "revision": "6c64f64141bd3066713e",
+    "url": "/single-perceptron/static/css/main.c0a19a9d.chunk.css"
+  },
+  {
+    "revision": "40b76bce9991c96dad309fe6d533b1b7",
+    "url": "/single-perceptron/index.html"
+  }
+];
